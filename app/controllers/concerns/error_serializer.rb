6a998e7f95711aa888b9654933928182cdec21f2
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2018-10-28 17:18:22 +0100

    Add title key to ErrorSerializer

--- app/controllers/concerns/error_serializer.rb
+++ app/controllers/concerns/error_serializer.rb
@@ -1,10 +1,12 @@
 module ErrorSerializer
   def self.serialize(object)
+    errors = []
     object.errors.messages.map do |field, errors|
       errors.map do |error_message|
         {
           status: 422,
-          source: {pointer: "/data/attributes/#{field}"},
+          title: 'Invalid attribute',
+          source: { pointer: "/data/attributes/#{field}" },
           detail: error_message
         }
       end
