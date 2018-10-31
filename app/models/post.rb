cb54846a860fee6f7baaf64c780793f03bd1bc44
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2018-10-31 15:31:12 +0100

    Add photo attachment in odel and serializer

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   serialize :tag_ids, Array
   validates :title, :body, :tag_ids, presence: true
+
+  has_one_attached :photo
 end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,9 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :tag_ids, :archived
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :title, :body, :tag_ids, :archived, :photo
+
+  def photo
+    url_for(object.photo) if object.photo.attached?
+  end
 end
