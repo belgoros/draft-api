0a9b0175cecc40d848ab3023ba587f090b7c7225
belgoros <s.cambour@gmail.com>
2018-11-01 16:40:37 +0100

    Use rails_blob_url, comment out variant use

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -4,7 +4,8 @@ class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :body, :tag_ids, :archived, :photo
 
   def photo
-    variant = object.photo.variant(resize: "100x100")
-    rails_representation_url(variant, only_path: true)
+    #variant = object.photo.variant(resize: "100x100")
+    #rails_representation_url(variant, only_path: true)
+    rails_blob_url(object.photo)
   end
 end
