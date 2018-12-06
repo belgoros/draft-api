5ee4f23c1e033c6722abcae61f8bd34de3ec5090
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2018-12-06 14:45:54 +0100

    Run rswag generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   resources :posts
   post '/rails/active_storage/direct_uploads' => 'direct_uploads#create'
 end
