1f5db6d19999b046ed6ff425d252640edec27852
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-03-13 16:29:38 +0100

    Remove auto-generated by swagger gem examples

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -14,38 +14,6 @@ RSpec.describe 'posts', type: :request do
         end
       end
     end
-    #post(summary: 'create post') do
-    #  response(200, description: 'successful') do
-    #    # You can add before/let blocks here to trigger the response code
-    #  end
-    #end
   end
 
-  #path '/posts/{id}' do
-  #  # You'll want to customize the parameter types...
-  #  parameter 'id', in: :path, type: :string
-  #  # ...and values used to make the requests.
-  #  let(:id) { '123' }
-  #
-  #  get(summary: 'show post') do
-  #    response(200, description: 'successful') do
-  #      # You can add before/let blocks here to trigger the response code
-  #    end
-  #  end
-  #  patch(summary: 'update post') do
-  #    response(200, description: 'successful') do
-  #      # You can add before/let blocks here to trigger the response code
-  #    end
-  #  end
-  #  put(summary: 'update post') do
-  #    response(200, description: 'successful') do
-  #      # You can add before/let blocks here to trigger the response code
-  #    end
-  #  end
-  #  delete(summary: 'delete post') do
-  #    response(200, description: 'successful') do
-  #      # You can add before/let blocks here to trigger the response code
-  #    end
-  #  end
-  #end
 end
